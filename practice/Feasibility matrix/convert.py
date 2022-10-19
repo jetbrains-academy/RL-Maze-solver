e0787f6f6ed4e308b52ace6dc3a0ecad9eb34179
Sofia Kolchanova <sofiia.kolchanova@gmail.com>
2022-10-19 15:09:21 +0200

    Some changes to code files shown to the student (first task)

--- practice/Feasibility matrix/convert.py
+++ practice/Feasibility matrix/convert.py
@@ -8,9 +8,7 @@ def find_reachable_neighbors(maze_, cell_):
 
 class Feasibility:
     def __init__(self, maze_):
-        self.cells = maze_.maze_grid.shape[0] * maze_.maze_grid.shape[1]
-        self.numbered_grid = np.arange(self.cells).reshape((maze_.maze_grid.shape[0], maze_.maze_grid.shape[1]))
-        self.F_matrix = np.zeros(shape=[self.cells, self.cells], dtype=int)
+        pass
 
     def get_neighbors(self, maze_):
         pass
--- practice/Feasibility matrix/learn.py
+++ practice/Feasibility matrix/learn.py
@@ -3,9 +3,13 @@ from convert import find_reachable_neighbors
 
 
 def get_poss_next_states(state, F, n_states):
+    # given a state s and a feasibility matrix F
+    # get list of possible next states
     pass
 
+
 def get_rnd_next_state(state, F, n_states):
+    # Given a state, pick a feasible next state.
     pass
 
 
@@ -14,10 +18,14 @@ class Agent:
         pass
 
     def set_rewards(self, maze, feasibility):
+        # Find the penultimate cell and set the highest reward for reaching the end of the maze.
         pass
 
     def train(self, F, max_epochs):
+        # Compute the Q matrix using the
+        # Bellman's equation: Q = [(1-a) * Q]  +  [a * (rt + (g * maxQ))]
         pass
 
     def walk(self):
+        # Walk to the goal from start using the Q matrix.
         pass
