4e1234aac56beb9e93211a77dfbd4e39746a6d05
Sofia Kolchanova <sofiia.kolchanova@gmail.com>
2022-11-09 18:36:03 +0100

    added example attributes to the init in Agent

--- practice/Introduction/learn.py
+++ practice/Introduction/learn.py
@@ -15,7 +15,9 @@ def get_rnd_next_state(state, F, n_states):
 
 class Agent:
     def __init__(self, feasibility, gamma, lrn_rate, maze, start_x, start_y):
-        pass
+        self.gamma = gamma
+        self.lrn_rate = lrn_rate
+        self.path = []
 
     def set_rewards(self, maze, feasibility):
         # Find the penultimate cell and set the highest reward for reaching the end of the maze.
