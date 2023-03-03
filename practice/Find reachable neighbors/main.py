2281daefb797b2540132a4e11023a102d7d46347
sofiiako <sofiia.kolchanova@gmail.com>
2023-03-03 13:20:37 +0100

    Updated main.py files throughout the project to be identical

--- practice/Find reachable neighbors/main.py
+++ practice/Find reachable neighbors/main.py
@@ -1,59 +1,46 @@
 from maze import Maze
 from convert import Feasibility
-from draw import make_movie
-from learn import Agent
+from draw import draw_maze
 import pandas as pd
 
 
-def my_print(Q):
-    labels = [str(x) for x in range(Q.shape[0])]
-    df = pd.DataFrame(Q, columns=labels, index=labels)
+# This function is needed to print a readable representation of the feasibility matrix.
+def my_print(matrix):
+    labels = [str(x) for x in range(matrix.shape[0])]
+    df = pd.DataFrame(matrix, columns=labels, index=labels)
     pd.set_option('display.max_rows', None)
     print(df.to_string())
 
 
 if __name__ == "__main__":
+    while True:
+        try:
+            dimensions = input('Enter maze dimensions separated by a space: ').split()
+            dimension1, dimension2 = int(dimensions[0]), int(dimensions[1])
+            if 0 < dimension1 and 0 < dimension2:
+                break
+            else:
+                print("Maze dimensions cannot be 0.")
+        except ValueError:
+            print("Dimensions must be integers.")
 
-    # gamma = 0.9
-    # lrn_rate = 0.5
-    # dimension1 = 5
-    # dimension2 = 5
-    dimension1 = int(input('Enter x dimension: '))
-    dimension2 = int(input('Enter y dimension: '))
-    start_x = int(input('Enter x coordinate of the maze start: '))
-    start_y = int(input('Enter y coordinate of the maze start: '))
-    gamma = float(
-        input('Enter the gamma value (0, 1] (aka the discount factor, influences the importance of future rewards): '))
-    lrn_rate = float(input(
-        'Enter the learning rate (0, 1] (larger values increase the influence of both current rewards and future rewards (explore) at the expense of past rewards (exploit)): '))
-    max_epochs = 1000
+    while True:
+        try:
+            start = input('Enter x and y coordinates of the maze start separated by a space: ').split()
+            start_x, start_y = int(start[0]), int(start[1])
+            if start_x <= dimension1 and start_y <= dimension2:
+                break
+            else:
+                print("Start coordinates should be inside the maze. Numbering is zero-based.")
+        except ValueError:
+            print("Start coordinates must be integers.")
 
     # Create the Maze
     maze = Maze(dimension1, dimension2, [start_x, start_y])
 
-    # Get the Feasibility Matrix
-    feasibility = Feasibility(maze)
+    # This will draw the maze and save it to the file maze.png
+    draw_maze(maze)
 
-    # Initialize the agent:
-    agent = Agent(feasibility, gamma, lrn_rate, maze, start_x, start_y)
 
-    print("Analyzing maze with RL Q-learning")
-    print("The F matrix:\n")
-    my_print(feasibility.F_matrix)
-
-    # Train the model:
-    agent.train(feasibility.F_matrix, max_epochs)
-    # train(feasibility.F_matrix, R_matrix, Q_matrix, gamma, lrn_rate, goal, n_states, max_epochs)
-    print("Done ")
-
-    print("The Q matrix is: \n ")
-    my_print(agent.Q)
-
-    print(f"Using Q to go from start to goal ({agent.goal})")
-
-    agent.walk()
-
-    # Create the dynamic visualization
-    make_movie(maze, feasibility, agent.path)
 
 
