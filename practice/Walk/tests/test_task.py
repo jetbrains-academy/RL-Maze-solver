b26d49b817041b35d85641bd2e79531d9b2fa2bf
sofiiako <sofiia.kolchanova@gmail.com>
2023-03-03 14:46:11 +0100

    Added a test to check that break is added to path.

--- practice/Walk/tests/test_task.py
+++ practice/Walk/tests/test_task.py
@@ -33,5 +33,26 @@ class TestCase(unittest.TestCase):
                 self.assertEqual(test_agent.path, agent.path)
 
             except TimeoutError as e:
-                self.fail(msg=f"TimeoutError after {timeoutlimit} seconds. Your implementation's execution does not seem "
-                              f"to end in a reasonable amount of time.")
+                self.fail(
+                    msg=f"TimeoutError after {timeoutlimit} seconds. Your implementation's execution does not seem "
+                        f"to end in a reasonable amount of time.")
+
+    def test_break_path(self):
+        for x in range(5):
+            dimension1, dimension2 = (3, 3)
+            start_x, start_y = (0, 0)
+            maze = Maze(dimension1, dimension2, [start_x, start_y])
+            feasibility = Feasibility(maze)
+            gamma = 0.1
+            lrn_rate = 0.1
+            max_epochs = 100
+            agent = Agent(feasibility, gamma, lrn_rate, maze, start_x, start_y)
+            try:
+                agent.train(feasibility.F_matrix, max_epochs)
+                agent.walk(maze, feasibility)
+                self.assertTrue('break' in agent.path, msg='You probably forgot to add "break" to self.path.')
+
+            except TimeoutError as e:
+                self.fail(
+                    msg=f"TimeoutError after {timeoutlimit} seconds. Your implementation's execution does not seem "
+                        f"to end in a reasonable amount of time.")
