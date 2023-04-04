552cc825a15f597615557ae717fccd4bef40e070
sofiiako <sofiia.kolchanova@gmail.com>
2023-04-04 13:21:42 +0200

    Dropped extra lessons and tasks

--- practice/Feasibility matrix/convert.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-
-class Feasibility:
-    def __init__(self, maze_):
-        self.cells = maze_.maze_grid.shape[0] * maze_.maze_grid.shape[1]
-        self.F_matrix = np.zeros(shape=[self.cells, self.cells], dtype=int)
-        self.numbered_grid = np.arange(self.cells).reshape((maze_.maze_grid.shape[0], maze_.maze_grid.shape[1]))
